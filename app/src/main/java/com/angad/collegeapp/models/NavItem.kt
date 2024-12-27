76a69fecabcc4f4c4108f232264d9754f9adf1ca
Angad Gupta <127626872+Angad-Gupta-2005@users.noreply.github.com>
2024-12-27 16:46:13 +0530

    Bottom Navigation functionality is done

--- app/src/main/java/com/angad/collegeapp/models/NavItem.kt
+++ app/src/main/java/com/angad/collegeapp/models/NavItem.kt
@@ -1,7 +1,5 @@
 package com.angad.collegeapp.models
 
-import androidx.compose.ui.graphics.vector.ImageVector
-
 //    For bottom navigation
 data class NavItem(
     val title: String,
--- app/src/main/java/com/angad/collegeapp/screens/BottomNav.kt
+++ app/src/main/java/com/angad/collegeapp/screens/BottomNav.kt
@@ -2,19 +2,20 @@ package com.angad.collegeapp.screens
 
 import android.widget.Toast
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -31,9 +32,15 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.angad.collegeapp.R
+import com.angad.collegeapp.models.BottomNavItem
 import com.angad.collegeapp.models.NavItem
+import com.angad.collegeapp.navigation.Routes
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -101,18 +108,85 @@ fun BottomNav(navController: NavController) {
             Scaffold(
                 topBar = {
                     TopAppBar(title = { Text(text = "College App") },
-                            navigationIcon = {
-                                IconButton(onClick = { scope.launch { drawerState.open() }}) {
-                                    Icon(imageVector = Icons.Default.Menu, contentDescription = null)
-                                }
+                        navigationIcon = {
+                            IconButton(onClick = { scope.launch { drawerState.open() }}) {
+                                Icon(imageVector = Icons.Default.Menu, contentDescription = null)
                             }
-                        )
+                        }
+                    )
+                },
+            //    For bottom navigation bar
+                bottomBar = {
+                    MyBottomNav(navController = navController1)
                 }
             ) { padding ->
-                Column(modifier = Modifier.padding(padding)) {
+                NavHost(
+                    navController = navController1,
+                    startDestination = Routes.Home.route,
+                    modifier = Modifier.padding(padding)
+                ){
+                    composable(route = Routes.Home.route) {
+                        Home()
+                    }
 
+                    composable(Routes.Gallery.route) {
+                        Gallery()
+                    }
+
+                    composable(Routes.AboutUs.route) {
+                        AboutUs()
+                    }
+
+                    composable(Routes.Faculty.route) {
+                        Faculty()
+                    }
                 }
             }
-        })
+        }
+    )
+}
 
+@Composable
+fun MyBottomNav(navController: NavController) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+
+//    List of item for bottom navigation
+    val list = listOf(
+        BottomNavItem("Home", R.drawable.home, Routes.Home.route),
+        BottomNavItem("Faculty", R.drawable.graduated, Routes.Faculty.route),
+        BottomNavItem("Gallery", R.drawable.image_gallery, Routes.Gallery.route),
+        BottomNavItem("About Us", R.drawable.info, Routes.AboutUs.route),
+    )
+
+    BottomAppBar {
+        list.forEach {
+        //    Getting the current route
+            val curRoute = it.route
+            val otherRoute =
+                try {
+                    backStackEntry.value!!.destination.route
+                } catch (e: Exception){
+                    Routes.Home.route
+                }
+
+            val selected = curRoute == otherRoute
+            
+            NavigationBarItem(
+                selected = selected,
+                onClick = { navController.navigate(it.route){
+                    popUpTo(navController.graph.findStartDestination().id) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                } },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = it.icon),
+                        contentDescription = it.title,
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+            )
+        }
+    }
 }
